4a1f2f63b724f5068572233bb6f0618e8c0551b7
ludralph <raphaelumoh@gmail.com>
2017-05-18 03:17:51 +0100

    Moved the models to a directory named app

---
+++ app/models/bookinstance.js
@@ -0,0 +1,20 @@
+import mongoose from 'mongoose';
+
+const Schema = mongoose.Schema;
+
+const BookInstanceSchema = Schema({
+  book: { type: Schema.ObjectId, ref: 'Book', required: true }, //reference to the associated book
+  imprint: {type: String, required: true},
+  status: {type: String, required: true, enum: ['Available', 'Loaned'], default: 'Available'},
+  due_back: {type: Date, default: Date.now},
+});
+
+// Virtual for bookinstance's URL
+BookInstanceSchema
+.virtual('url')
+.get(function () {
+  return `/catalog/bookinstance/${this._id}`;
+});
+
+//Export model
+export default mongoose.model('BookInstance', BookInstanceSchema);
