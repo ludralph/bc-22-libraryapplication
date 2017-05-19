8abcc7ea6b4e67f0a530eaa659a1718efb4248fe
ludralph <raphaelumoh@gmail.com>
2017-05-19 10:52:04 +0100

    updated routes and header template

--- app/routes.js
+++ app/routes.js
@@ -37,7 +37,7 @@ module.exports = function(app, passport) {
     });
 
     // LOGOUT ==============================
-    app.get('/logout', function(req, res) {
+    app.get('/logout', isAuthenticated, function(req, res) {
         req.logout();
         res.redirect('/');
     });
